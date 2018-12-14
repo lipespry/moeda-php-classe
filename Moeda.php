49e4cfc2d67aa0eda3717b2e3238e2caa5b86c22
lipespry <felipe_moraes_bmx@hotmail.com>
2018-12-14 10:21:39 -0200

    leia-me atualizado

--- Moeda.php
+++ Moeda.php
@@ -5,13 +5,13 @@
  *
  * Exemplo de uso:
  *
- * $moeda = new Moeda('R$ 1.234.567,89');
- * $moeda->getBRL(true, 0);      // Retorna: R$ 1.234.568 (string)
- * $moeda->getBRL(false, 0);     // Retorna: 1.234.568 (string)
- * $moeda->getBRL(true, 2);      // Retorna: R$ 1.234.567,89 (string)
- * $moeda->getEUR(true, 2);      // Retorna: 1 234 567.89 € (string)
- * $moeda->getUSD(true, 2);      // Retorna: US$ 1,234,567.89 (string)
- * $moeda->getFloat(1);          // Retorna: 1234567.9 (float)
+ *     $moeda = new Moeda('R$ 1.234.567,89');
+ *     $moeda->getBRL(true, 0);      // Retorna: R$ 1.234.568 (string)
+ *     $moeda->getBRL(false, 0);     // Retorna: 1.234.568 (string)
+ *     $moeda->getBRL(true, 2);      // Retorna: R$ 1.234.567,89 (string)
+ *     $moeda->getEUR(true, 2);      // Retorna: 1 234 567.89 € (string)
+ *     $moeda->getUSD(true, 2);      // Retorna: US$ 1,234,567.89 (string)
+ *     $moeda->getFloat(1);          // Retorna: 1234567.9 (float)
  *
  * @author Felipe "LipESprY" Moraes <felipemdeoliveira@live.com>
  * @see https://github.com/lipespry/moeda-php-classe
